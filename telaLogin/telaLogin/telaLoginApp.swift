e0b691a9ae671f31056288bc0d583e560294550b
Gbr1elResende <gabrielresende317@gmail.com>
2025-04-01 09:38:37 -0300

    feature: tela de login

---
+++ telaLogin/telaLogin/telaLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  telaLoginApp.swift
+//  telaLogin
+//
+//  Created by COTEMIG on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct telaLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
